2b4f97f1badf2934ff27e605aa068a0c8a9ab3d3
Bart <bart@vandelint.net>
2026-02-21 19:25:17 +0100

    Remove unused scripts

--- scripts/create_test_sams.jl
+++
@@ -1,7 +0,0 @@
-# SPDX-FileCopyrightText: 2025 Bart van de Lint <bart@vandelint.net>
-#
-# SPDX-License-Identifier: MPL-2.0
-
-using SymbolicAWEModels
-sams = SymbolicAWEModels.create_default_models(; prn=true)
-nothing
