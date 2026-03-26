fd609a0a54ab67b4fe335091c401e06f80a53208
Uwe Fechner <u.fechner-1@tudelft.nl>
2026-03-26 15:23:23 +0100

    Add test_for_precompile.jl

---
+++ test/test_for_precompile.jl
@@ -0,0 +1,45 @@
+# TODO add code that always is needed
+
+using GLMakie
+using SymbolicAWEModels
+import SymbolicAWEModels: Point  # resolve ambiguity with GLMakie
+
+set_data_path(joinpath(dirname(@__DIR__), "data"))
+set = Settings("base/system.yaml")
+set.v_wind = 0
+
+points = [
+    Point(:anchor, [2, 0, 5], STATIC),
+    Point(:mass, [2, 0, 2], DYNAMIC; extra_mass=1.0),
+]
+segments = [
+    Segment(:spring, :anchor, :mass,
+            500.0, 50.0, 0.005; l0=4.0),
+]
+transforms = [
+    Transform(:tf, -deg2rad(90), 0, 0;
+              base_pos=[2, 0, 5], base_point=:anchor,
+              rot_point=:mass),
+]
+
+sys = SystemStructure("hanging_mass", set;
+                      points, segments, transforms)
+sam = SymbolicAWEModel(set, sys)
+init!(sam; remake=false)
+
+n_steps = 30
+logger = Logger(sam, n_steps)
+sys_state = SysState(sam)
+
+for i in 1:n_steps
+    next_step!(sam)
+    update_sys_state!(sys_state, sam)
+    sys_state.time = i / set.sample_freq
+    log!(logger, sys_state)
+end
+
+save_log(logger, "_hanging_mass")
+
+syslog = load_log("_hanging_mass")
+scene = replay(syslog, sam.sys_struct)
+display(scene)
