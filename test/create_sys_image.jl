1ba4c7edd302b1ce70f8a3c29e162ee29f2c465e
Uwe Fechner <u.fechner-1@tudelft.nl>
2026-04-01 16:10:45 +0200

    Better warning/ error msg

--- test/create_sys_image.jl
+++ test/create_sys_image.jl
@@ -16,6 +16,41 @@ using NonlinearSolve, OrdinaryDiffEqBDF, OrdinaryDiffEqCore, OrdinaryDiffEqNonli
 # --- Open Source AWE Packages ---
 using AtmosphericModels, KiteUtils, VortexStepMethod
 
+GC.gc(true)
+let mem = Sys.free_memory() / 1024^2
+    @info "Free memory: $(round(mem; digits=1)) MB"
+    swap_gb = 0.0
+    if Sys.islinux()
+        swap_info = read(`swapon --show --bytes --noheadings`, String)
+        if !isempty(strip(swap_info))
+            swap_size = sum(parse(Int, split(line)[3]) for line in split(strip(swap_info), '\n') if !isempty(line))
+            swap_gb = swap_size / 1024^3
+            @info "Swap size: $(round(swap_gb; digits=1)) GB"
+        else
+            @info "No swap configured"
+        end
+    elseif Sys.iswindows()
+        pf_info = read(`wmic pagefile list /format:list`, String)
+        m = match(r"AllocatedBaseSize=(\d+)", pf_info)
+        if m !== nothing
+            swap_gb = parse(Int, m.captures[1]) / 1024
+            @info "Page file size: $(round(swap_gb; digits=1)) GB"
+        else
+            @info "Page file size: unknown"
+        end
+    end
+    if haskey(ENV, "JULIA_IMAGE_THREADS")
+        @info "JULIA_IMAGE_THREADS: $(ENV["JULIA_IMAGE_THREADS"])"
+    else
+        free_gb = mem / 1024
+        if free_gb + swap_gb < 36.0
+            @error "JULIA_IMAGE_THREADS is not defined and total available memory ($(round(free_gb + swap_gb; digits=1)) GB free RAM + swap) is less than 36 GB. System image creation may fail!"
+        else
+            @info "JULIA_IMAGE_THREADS not defined!"
+        end
+    end
+end
+
 @info "Creating sysimage ..."
 PackageCompiler.create_sysimage(
     [:Pkg, :TOML, :DocStringExtensions, :LinearAlgebra, :Parameters, :Printf, :Serialization, :SHA, :CodecZlib, :Tar, :Statistics, :Suppressor, :Timers, :GLMakie, :ModelingToolkit, :ControlSystemsBase, :RecipesBase, :StaticArrays, :SymbolicIndexingInterface, :NonlinearSolve, :OrdinaryDiffEqBDF, :OrdinaryDiffEqCore, :OrdinaryDiffEqNonlinearSolve, :SteadyStateDiffEq, :AtmosphericModels, :KiteUtils, :VortexStepMethod];
